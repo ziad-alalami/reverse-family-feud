7e53ef52b36f01be4cfdcf52ddffb44c75a286db
ziad <ziadtareq1123@gmail.com>
2026-03-18 15:53:20 +0300

    clean up all unused old files and fixing files

--- fix_categories.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('backend/app/routes/categories.py', 'utf8');
-
-code = code.replace(
-    '    return {"category_id": category_id, "rank": rank, "removed": True}',
-    '    await broadcast_scores_update(category.game_id)\n    return {"category_id": category_id, "rank": rank, "removed": True}'
-);
-
-fs.writeFileSync('backend/app/routes/categories.py', code);
